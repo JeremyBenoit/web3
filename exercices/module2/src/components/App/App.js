5eec64975c3887a7e1ab7ff5ba8e4e59dbae541f
Jeremy Benoit <jeremy.benoit@student.vinci.be>
2022-09-23 10:15:33 +0200

    module2 : localStorage

--- exercices/module2/src/components/App/App.js
+++ exercices/module2/src/components/App/App.js
@@ -3,30 +3,34 @@ import Display from "components/Display/Display";
 import {useState} from "react";
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+    const defaultCounter = JSON.parse(localStorage.getItem("counter")) ?? 0
+    const [counter, setCounter] = useState(defaultCounter)
 
-  const changeCount = (delta) => setCounter(counter+delta)
+    const changeCount = async (delta) => {
+        setCounter(counter + delta)
+        localStorage.setItem('counter', JSON.stringify(counter+delta))
+    }
 
-  return (
-    <div>
-      <Display counter={counter}/>
-      <Button
-          changeCount={changeCount}
-          text={'Plus'}
-          delta={+1}
-      />
-      <Button
-          changeCount={changeCount}
-          text={'Zero'}
-          delta={-counter}
-      />
-      <Button
-          changeCount={changeCount}
-          text={'Minus'}
-          delta={-1}
-      />
-    </div>
-  )
+    return (
+        <div>
+            <Display counter={counter}/>
+            <Button
+                changeCount={changeCount}
+                text={'Plus'}
+                delta={+1}
+            />
+            <Button
+                changeCount={changeCount}
+                text={'Zero'}
+                delta={-counter}
+            />
+            <Button
+                changeCount={changeCount}
+                text={'Minus'}
+                delta={-1}
+            />
+        </div>
+    )
 }
 
 export default App
