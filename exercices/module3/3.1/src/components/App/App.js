8e62d64a173a80c5e8c252dc07c5a10637438b21
Jeremy Benoit <jeremy.benoit@student.vinci.be>
2022-10-04 18:12:11 +0200

    module3 : loading

--- exercices/module3/3.1/src/components/App/App.js
+++ exercices/module3/3.1/src/components/App/App.js
@@ -1,23 +1,30 @@
 import { useState } from 'react'
 import Button from "components/Button/Button";
 import Statistics from "components/Statistics/Statistics";
+import Loading from "components/Loading/Loading";
 
 const App = () => {
     // save clicks of each button to its own state
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
+    const [loading, setLoading] = useState(false)
 
-    return (
-        <div>
-            <h1>give feedback</h1>
-            <Button text="good" incrementCounter={() => setGood(good+1)}/>
-            <Button text="neutral" incrementCounter={() => setNeutral(neutral+1)}/>
-            <Button text="bad" incrementCounter={() => setBad(bad+1)}/>
-            <h1>statistics</h1>
-            <Statistics good={good} neutral={neutral} bad={bad}/>
-        </div>
-    )
+    setTimeout(() => setLoading(true),3000)
+
+    if(loading)
+        return (
+            <div>
+                <h1>give feedback</h1>
+                <Button text="good" incrementCounter={() => setGood(good+1)}/>
+                <Button text="neutral" incrementCounter={() => setNeutral(neutral+1)}/>
+                <Button text="bad" incrementCounter={() => setBad(bad+1)}/>
+                <h1>statistics</h1>
+                <Statistics good={good} neutral={neutral} bad={bad}/>
+            </div>
+        )
+    else
+        return <Loading/>
 }
 
 export default App
---
+++ exercices/module3/3.1/src/components/Loading/Loading.jsx
@@ -0,0 +1,6 @@
+import "components/Loading/Loading.css"
+
+const Loading = () => {
+    return <h1 className="Loading">Un instant S.V.P.</h1>
+}
+export default Loading
